4df88bad74a88a0c435d69cd8a4b42c078842282
Grace <grace.h.gamble@gmail.com>
2016-05-23 23:05:36 +0000

    Last Update to Functions Practice

--- FunctionsPractice.js
+++ FunctionsPractice.js
@@ -83,6 +83,49 @@ console.log(excite("go"));
 */
 
 
+
 //Part 8
+/*
+function sun(string) {
+    var n = string.indexOf("sun");
+        if(n === -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+}
+console.log(sun("sundries"));
+console.log(sun("asunder"));
+console.log(sun("catapult"));
+*/
 
 
+
+//Part 9
+/*
+function tiny (number) {
+    if(number > 0 && number < 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(tiny(0.3));
+console.log(tiny(14));
+console.log(tiny(-5));
+*/
+
+
+
+//Part 10
+/*
+function getSeconds(string) {
+    var minutes = string.substring(0, 2);
+    var seconds = string.substring(3, 5);
+    return(+minutes * 60 + +seconds);
+}
+console.log(getSeconds('01:30'));
+console.log(getSeconds('10:25'));
+*/
