317d84a6f3171a9311d242b2147179662d2628a0
Grace <grace.h.gamble@gmail.com>
2016-05-23 18:03:58 +0000

    First upate to Functions Practice Part 1

---
+++ FunctionsPractice.js
@@ -0,0 +1,37 @@
+//Part 1
+/*
+function TripleFive () {
+    for(var i = 0; i < 3; i++) {
+        console.log("Five!");
+    }
+}
+
+TripleFive();
+*/
+
+
+
+//Part 2
+/*
+function lastLetter (name) {
+    return (name.charAt(name.length-1));
+}
+
+console.log(lastLetter('hello'));
+console.log(lastLetter('island'));
+*/
+
+
+
+//Part 3
+/*
+function Square (number) {
+    return (number * number);
+}
+
+console.log(Square(3));
+console.log(Square(5));
+*/
+
+
+//Part 4
