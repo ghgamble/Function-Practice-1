77f6efd81a1934556a36eeaeda9f099e87eb0c86
Grace <grace.h.gamble@gmail.com>
2016-05-23 18:27:01 +0000

    Second Update to Functions Practice Part 1

--- FunctionsPractice.js
+++ FunctionsPractice.js
@@ -34,4 +34,44 @@ console.log(Square(5));
 */
 
 
+
 //Part 4
+/*
+function Negate (number) {
+    return (number * -1);
+}
+console.log(Negate(5));
+console.log(Negate(-8));
+*/
+
+
+
+//Part 5
+/*
+function toArray(a, b, c) {
+    return ([a, b, c]);
+}
+console.log(toArray(1, 4, 5));
+console.log(toArray(8, 9, 10));
+*/
+
+
+
+//Part 6
+/*
+function startsWithA(string) {
+    if(string.charAt(0) === "a") {
+        return("True");
+    }
+    else{
+        return("False");
+    }
+}
+console.log(startsWithA("aardvark"));
+console.log(startsWithA("bear"));
+*/
+
+
+
+//Part 7
+
